3997d7d089b856e08eaca78a8b09d7e45e205321
HarshDThoriya <harshthoriya.ce@gmail.com>
2024-11-08 12:40:17 -0800

    euclidean algorithm

---
+++ Euclidean Algorithm/EuclideanAlgorithm.java
@@ -0,0 +1,25 @@
+public class EuclideanAlgorithm {
+
+    public static int findGcd(int n1, int n2){
+        while(n1> 0 && n2>0){
+            if(n1>n2){
+                n1 = n1 % n2;
+            }
+            else{
+                n2 = n2 % n1;
+            }
+            System.out.println(n1 + " " + n2);
+        }
+
+        return n1 == 0 ? n2 : n1;
+    }
+
+    public static void main(String[] args) {
+        int n1 = 20, n2 = 15;
+
+        int gcd = findGcd(n1, n2);
+
+        System.out.println("GCD of " + n1 + " and " + n2 + " is: " + gcd);
+    }
+
+}
