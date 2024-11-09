256939e613af54fa6a9c3fe77b4fcaf86e745571
HarshDThoriya <harshthoriya.ce@gmail.com>
2024-11-09 09:50:29 -0800

    multiple

---
+++ Second Largest/SecondLargest.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        if (n < 2) {
+            System.out.println("Second Largest element in array is: " + -1);
+            System.exit(0);
+        }
+
+        int max = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                secondLargest = max;
+                max = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != max) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Second Largest element in array is: " + secondLargest);
+    }
+}
