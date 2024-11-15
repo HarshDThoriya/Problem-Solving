63a0e4fc128fc5a2ac72abc945f3cf8de498471a
HarshDThoriya <harshthoriya.ce@gmail.com>
2024-11-15 11:22:17 -0800

    Max Subarray Sum - Kadane's Algorithm

---
+++ Maximum Subarray Sum/MaxSubArraySum.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class MaxSubArraySum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sc.close();
+
+        long maxi = Long.MIN_VALUE; 
+        long sum = 0;
+
+        int start = 0;
+        int ansStart = -1, ansEnd = -1;
+        for (int i = 0; i < n; i++) {
+
+            if (sum == 0) start = i; 
+
+            sum += arr[i];
+
+            if (sum > maxi) {
+                maxi = sum;
+
+                ansStart = start;
+                ansEnd = i;
+            }
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        System.out.print("The subarray is: [");
+        for (int i = ansStart; i <= ansEnd; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print("] and Sum is "+ maxi);
+    }
+}
