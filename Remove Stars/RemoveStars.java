683ffb2678e33330cb178a83347bcca7bf04b9af
HarshDThoriya <harshthoriya.ce@gmail.com>
2024-11-24 14:51:07 -0800

    Remove Stars

---
+++ Remove Stars/RemoveStars.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class RemoveStars {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        StringBuilder sb = new StringBuilder("");
+        
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) != '*'){
+                stack.push(s.charAt(i));
+            }
+            else{
+                stack.pop();
+            }
+        }
+
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+
+        System.out.println(sb.reverse().toString());
+    }
+}
