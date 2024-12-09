0aecae69fb41aaa6313579a3b73abc571cc35040
HarshDThoriya <harshthoriya.ce@gmail.com>
2024-12-09 15:29:43 -0800

    Binary Tree

--- Binary Tree/BinaryTree.java
+++ Binary Tree/BinaryTree.java
@@ -15,6 +15,7 @@ class Node {
 public class BinaryTree {
 
     static Queue<Node> queue = new LinkedList<>();
+    static Stack<Node> stack = new Stack<>();
 
     static void preOrderTraversal(Node root) {
         if (root == null) {
@@ -27,6 +28,20 @@ public class BinaryTree {
         preOrderTraversal(root.right);
     }
 
+    static void IterativePreOrderTraversal() {
+        while (!stack.isEmpty()) {
+            Node current = stack.pop();
+
+            System.out.print(current.data + " ");
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+        }
+    }
+
     static void postOrderTraversal(Node root) {
         if (root != null) {
             postOrderTraversal(root.left);
@@ -35,11 +50,55 @@ public class BinaryTree {
         }
     }
 
+    static void IterativePostOrderTraversal2Stacks(Node root){
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
+
+        Node current = null;
+        stack1.push(root);
+
+        while(!stack1.isEmpty()){
+            current = stack1.pop();
+            
+            stack2.push(current);
+
+            if(current.left != null){
+                stack1.push(current.left);
+            }
+
+            if(current.right != null){
+                stack1.push(current.right);
+            }
+        }
+
+        while(!stack2.isEmpty()){
+            System.out.print(stack2.pop().data + " ");
+        }
+    }
+
+    static void IterativePostOrderTraversal1Stacks(Node root){
+
+    }
+
     static void inOrderTraversal(Node root) {
         if (root != null) {
             inOrderTraversal(root.left);
             System.out.print(root.data + " ");
             inOrderTraversal(root.right);
+        }
+    }
+
+    static void IterativeInOrderTraversal(Node root){
+        Node current = root;
+        while(!stack.isEmpty() || current != null){
+            while(current != null){
+                stack.push(current);
+                current = current.left;
+            }           
+            
+            current = stack.pop();
+            System.out.print(current.data + " ");
+            current = current.right;
         }
     }
 
@@ -64,33 +123,33 @@ public class BinaryTree {
     }
 
     static int heightOfBinaryTree(Node root) {
-        if(root == null){
+        if (root == null) {
             return 0;
         }
 
-        int lHeight = isBalancedTree(root.left);
-        int rHeight = isBalancedTree(root.right);
+        int lHeight = heightOfBinaryTree(root.left);
+        int rHeight = heightOfBinaryTree(root.right);
 
-        return Math.max(lHeight,rHeight)+1;
+        return Math.max(lHeight, rHeight) + 1;
     }
 
-    static int isBalancedTree(Node root){
-        if(root == null){
+    static int isBalancedTree(Node root) {
+        if (root == null) {
             return 0;
         }
 
         int lHeight = isBalancedTree(root.left);
         int rHeight = isBalancedTree(root.right);
 
-        if(lHeight == -1 || rHeight == -1){
-            return -1;
-        } 
-
-        if(Math.abs(lHeight - rHeight) > 1){
+        if (lHeight == -1 || rHeight == -1) {
             return -1;
         }
 
-        return Math.max(lHeight,rHeight)+1;
+        if (Math.abs(lHeight - rHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(lHeight, rHeight) + 1;
     }
 
     public static void main(String[] args) {
@@ -106,10 +165,24 @@ public class BinaryTree {
         root.right.right.right = new Node(10);
 
         preOrderTraversal(root);
+        stack.push(root);
+        System.out.println();
+        IterativePreOrderTraversal();
+
         System.out.println();
         postOrderTraversal(root);
         System.out.println();
+        IterativePostOrderTraversal2Stacks(root);
+        System.out.println();
+        IterativePostOrderTraversal1Stacks(root);
+        System.out.println();
+
         inOrderTraversal(root);
+        stack.clear();
+        System.out.println();
+        IterativeInOrderTraversal(root);
+        
+
         System.out.println();
         queue.add(root);
         levelOrderTraversal();
