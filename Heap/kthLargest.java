21db79b4a395765e3ae7d502b79cb94b7e7bbde0
HarshDThoriya <harshthoriya.ce@gmail.com>
2025-01-07 10:57:22 -0800

    name change

--- Heap/kthLargest.java
+++ Heap/kthLargest.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 public class kthLargest {
     public static void main(String[] args) {
-        int[] arr = new int[]{2,3,4,2,1,6,4,2,5};
+        int[] nums = new int[]{2,3,4,2,1,6,4,2,5};
 
         int k = 5;
         
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
 
-        for(int i=0; i<arr.length; i++){
-            pq.add(arr[i]);
+        for(int i=0; i<nums.length; i++){
+            pq.add(nums[i]);
         }
 
         for(int i=0; i<k-1; i++){
