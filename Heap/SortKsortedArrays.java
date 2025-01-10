5cb2e07792de76a3392881d6f5cab85fab038df2
HarshDThoriya <harshthoriya.ce@gmail.com>
2025-01-10 12:23:14 -0800

    Sort K Sorted Arrays Optimized

--- Heap/SortKsortedArrays.java
+++ Heap/SortKsortedArrays.java
@@ -1,20 +1,51 @@
+package Heap;
+
 import java.util.*;
+
+class HeapItem implements Comparable<HeapItem> {
+    int[] arr;
+    int index;
+
+    public HeapItem(int[] arr, int index) {
+        this.arr = arr;
+        this.index = index;
+    }
+
+    @Override
+    public int compareTo(HeapItem h) {
+        if (arr[index] > h.arr[h.index])
+            return 1;
+        else if (arr[index] < h.arr[h.index])
+            return -1;
+        else
+            return 0;
+    }
+}
 
 public class SortKsortedArrays {
     public static void main(String[] args) {
-        int[][] arr = new int[][]{{1,2,10},{4,5,6},{7,8,9}};
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int[][] arr = new int[][] { { 1, 2, 10 }, { 4, 5, 6 }, { 1, 8, 9 } };
+        ArrayList<Integer> al = new ArrayList<>();
 
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr.length; j++){
-                pq.add(arr[j][i]);
+        PriorityQueue<HeapItem> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            pq.add(new HeapItem(arr[i], 0));
+        }
+
+        while (!pq.isEmpty()) {
+            HeapItem current = pq.remove();
+
+            al.add(current.arr[current.index]);
+
+            if(current.index < current.arr.length - 1){
+                current.index++;
+                pq.add(current);
             }
         }
 
-        while(!pq.isEmpty()){
-            System.out.println(pq.poll());
+        for(int i=0; i<al.size(); i++){
+            System.out.println(al.get(i));
         }
-        
     }
 }
