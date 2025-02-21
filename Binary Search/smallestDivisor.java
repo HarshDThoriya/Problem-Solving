8f1faa9aa2aa5d209f2dbe668c51bc5d54581cae
HarshDThoriya <harshthoriya.ce@gmail.com>
2025-02-20 18:43:44 -0800

    improvement on smallest divisor

--- Binary Search/smallestDivisor.java
+++ Binary Search/smallestDivisor.java
@@ -1,48 +1,23 @@
 public class smallestDivisor {
 
-    static int findSum(int[] nums, int divisor){
-        int sum = 0;
-
-        for(int i=0; i<nums.length; i++){
-            sum+= (int)Math.ceil((float)nums[i]/divisor);
-        }
-        return sum;
-    }
     public static void main(String[] args) {
-        int limit = 6;
+        int threshold = 6;
         int[] nums = new int[]{1,2,5,9};
-
-        int maxElement = Integer.MIN_VALUE;
-
-        for(int i=0;i<nums.length;i++){ 
-            if(maxElement < nums[i]){
-                maxElement = nums[i];
-            }
+        
+        int low = 1, high = 1000000;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(result(nums, mid, threshold)) high = mid - 1;
+            else low = mid + 1;
         }
-
-        int low = 1;
-        int high = maxElement;
-
-        int minElement = Integer.MAX_VALUE;
-
-        while(low<=high){
-            int mid = (low+high)/2;
-            
-            int sum = findSum(nums, mid);
-
-            
-            
-            if(sum > limit){
-                low = mid + 1;
-            }
-            else{
-                minElement = mid;
-                high = mid - 1;
-            }
-            
-        }
-
-
-        System.out.println(minElement);
+        System.out.println(low);
     }
+    private static boolean result(int nums[], int divisor, int threshold){
+        int ans = 0;
+        for(int i : nums){
+            ans += Math.ceilDiv(i, divisor);
+        }
+        return ans <= threshold;
+    }
+    
 }
